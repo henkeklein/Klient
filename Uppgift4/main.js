0873a96463238f9f874b2690ef60adf202ddbfde
Henrik Klein <henrik.klein12@gmail.com>
2016-11-23 10:02:19 +0100

    new

--- Uppgift4/main.js
+++ Uppgift4/main.js
@@ -1,10 +1,10 @@
-'use strict';
+"use strict";
 var omdbAPI = new XMLHttpRequest();
-var query;
+
 document.getElementById("search-form").onsubmit = function(event) {
-    query = decodeURI(document.getElementById("string").value);
+    var query = decodeURI(document.getElementById("string").value);
     var omdbURL = "http://www.omdbapi.com/?s=" + query;
-    omdbAPI.open("GET", omdbURL, true);
+    omdbAPI.open("get", omdbURL, true);
     omdbAPI.send();
     if(query == ''){
       checkIfEmpty(query);
@@ -14,27 +14,26 @@ document.getElementById("search-form").onsubmit = function(event) {
     }
     event.preventDefault();
 };
-
 function loadMovie(){
-omdbAPI.addEventListener("load", function(event) {
+omdbAPI.addEventListener("load", function() {
     var i = 0;
     var movies = document.getElementById("movies");
     var result = JSON.parse(this.responseText);
     $('#movies').empty();
     console.log(result);
 
-    if (this.readyState == 4 && this.status == 200 && result.Response == 'true'){
-      while(i < result.Search.length){
-        var movies = document.getElementById("movies");
-        var list = document.createElement("li");
-        list.appendChild(document.createTextNode(result.Search[i].Title + " [" + result.Search[i].Year + "]"));
-        $('#movies').append(list);
-        i++;
-      }
-    } else {
-      alert("Something went wrong");
-    }
+    if (this.readyState == 4 && this.status == 200 && result.Response == "True") {
+        while (i < result.Search.length) {
+            var movies = document.getElementById("movies");
+            var list = document.createElement("li");
+            list.appendChild(document.createTextNode(result.Search[i].Title + " [" + result.Search[i].Year + "]"));
+            $('#movies').append(list);
+            i++;
+        }
 
+    } else {
+        alert("Movie where not found");
+    }
 });
 }
 function checkIfEmpty(field) {
