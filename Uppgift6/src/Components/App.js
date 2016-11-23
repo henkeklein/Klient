f8dd277781ef379c1ac78ebf1bcfe309cb106485
Henrik Klein <henrik.klein12@gmail.com>
2016-11-23 10:40:35 +0100

    start

---
+++ Uppgift6/src/Components/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import './App.css';
+import Header from './Header';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+      <h1>Welcome To My Mashup</h1>
+      <Header />
+      </div>
+    );
+  }
+}
+
+export default App;
